e47903a02742984b49491e638fe212f60ad32544
Dipak Pokharel <104352102+dipakPJS@users.noreply.github.com>
2024-02-27 10:42:51 +0545

    Update Hero.tsx

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -5,18 +5,16 @@ import '../../app/globals.css'
 const Hero = () => {
   return (
 <div className="relative flex flex-col h-screen justify-center items-center text-center" id="about-me">
-  <video
-    autoPlay
-    muted
-    loop
-    className="rotate-180 absolute top-[-450px] left-1/2 transform -translate-x-1/2 z-10 object-cover"
-    style="width: 80vw;"
-  >
-    <source src="/sphere.mp4" type="video/mp4" />
-  </video>
-  <HeroContent />
-</div>
-
+      <video
+        autoPlay
+        muted
+        loop
+        className="rotate-180 absolute top-[-450px] left-1/2 transform -translate-x-1/2 h-full w-auto z-10 object-cover video-sphere"
+      >
+        <source src="/sphere.mp4" type="video/mp4" />
+      </video>
+      <HeroContent />
+    </div>
 
 
   );
