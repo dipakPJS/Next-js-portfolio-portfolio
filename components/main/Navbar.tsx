80971d6cfc5b34d03f0fa5820bb60eaa3a569579
Dipak Pokharel <104352102+dipakPJS@users.noreply.github.com>
2024-02-27 06:44:58 +0545

    Update Navbar.tsx

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -4,52 +4,53 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#130f40]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
-      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
-        <a
-          href="#about-me"
-          className="h-auto w-auto flex flex-row items-center ml-[40px]"
-        >
-          <Image
-            src="/dipakLogo.png"
-            alt="logo"
-            width={70}
-            height={70}
-            className="cursor-pointer hover:animate-slowspin"
-          />
+     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#130f40]/50 bg-[#03001417] backdrop-blur-md z-50 px-5 md:px-10">
+  <div className="w-full h-full flex flex-row items-center justify-between m-auto px-1 md:px-[10px]">
+    <a
+      href="#about-me"
+      className="h-auto w-auto flex flex-row items-center ml-2 md:ml-[40px]"
+    >
+      <Image
+        src="/dipakLogo.png"
+        alt="logo"
+        width={70}
+        height={70}
+        className="cursor-pointer hover:animate-slowspin"
+      />
 
-          <span className="font-bold ml-[1px] hidden md:block text-gray-300">
-            ipak Dev
-          </span>
+      <span className="font-bold ml-1 md:ml-[1px] text-sm md:text-lg text-gray-300">
+        Dipak Dev
+      </span>
+    </a>
+
+    <div className="w-full md:w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
+      <div className="flex items-center justify-between w-full h-auto border border-[#FA571C] bg-[#0300145e] mr-2 md:mr-[15] ml-2 md:ml-[20px] px-2 md:px-[20px] py-1 md:py-[10px] rounded-full text-sm md:text-base text-gray-200">
+        <a href="#about-me" className="cursor-pointer">
+          About me
         </a>
-
-        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
-          <div className="flex items-center justify-between w-full h-auto border border-[#FA571C] bg-[#0300145e] mr-[15] ml-[20px] px-[20px] py-[10px] rounded-full text-gray-200">
-            <a href="#about-me" className="cursor-pointer">
-              About me
-            </a>
-            <a href="#skills" className="cursor-pointer">
-              Skills
-            </a>
-            <a href="#projects" className="cursor-pointer">
-              Projects
-            </a>
-          </div>
-        </div>
-
-        <div className="flex flex-row gap-5">
-          {Socials.map((social) => (
-            <Image
-              src={social.src}
-              alt={social.name}
-              key={social.name}
-              width={24}
-              height={24}
-            />
-          ))}
-        </div>
+        <a href="#skills" className="cursor-pointer">
+          Skills
+        </a>
+        <a href="#projects" className="cursor-pointer">
+          Projects
+        </a>
       </div>
     </div>
+
+    <div className="flex flex-row gap-2 md:gap-5">
+      {Socials.map((social) => (
+        <Image
+          src={social.src}
+          alt={social.name}
+          key={social.name}
+          width={24}
+          height={24}
+        />
+      ))}
+    </div>
+  </div>
+</div>
+
   );
 };
 
