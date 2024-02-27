d45db937cb1055cb9756daeb4d08faa104126e19
Dipak Pokharel <104352102+dipakPJS@users.noreply.github.com>
2024-02-27 07:37:27 +0545

    Update HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -12,63 +12,63 @@ import Image from "next/image";
 
 const HeroContent = () => {
   return (
+  <motion.div
+  initial="hidden"
+  animate="visible"
+  className="flex flex-row items-center justify-center px-5 md:px-20 mt-10 md:mt-40 w-full z-[20]"
+>
+  <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
     <motion.div
-      initial="hidden"
-      animate="visible"
-      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
+      variants={slideInFromTop}
+      className="Welcome-box py-2 md:py-8 px-2 md:px-7 border border-orange-500 opacity-[0.9]"
     >
-      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
-        <motion.div
-          variants={slideInFromTop}
-          className="Welcome-box py-[8px] px-[7px] border border-orange-500 opacity-[0.9]"
-        >
-          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text text-[13px]">
-           Creative Developer Portfolio
-          </h1>
-        </motion.div>
-
-        <motion.div
-          variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
-        >
-          <span>
-            Providing
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-orange-700">
-              {" "}
-              the best{" "}
-            </span>
-            project exprience
-          </span>
-        </motion.div>
-
-        <motion.p
-          variants={slideInFromLeft(0.8)}
-          className="text-lg text-gray-400 my-5 max-w-[600px]"
-        >
-          I&apos;m a Full Stack Web Developer with experience in Website,
-          Mobile, and Software development. Check out my projects and skills.
-        </motion.p>
-        <motion.a
-          variants={slideInFromLeft(1)}
-          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
-        >
-          Learn More!
-        </motion.a>
-      </div>
-
-      <motion.div
-        variants={slideInFromRight(0.8)}
-        className="w-full h-full flex justify-center items-center"
-      >
-        <Image
-          src="/mainIconsdark.svg"
-          alt="work icons"
-          height={650}
-          width={650}
-        />
-      </motion.div>
+      <SparklesIcon className="text-[#b49bff] mr-2 md:mr-10 h-4 md:h-5 w-4 md:w-5" />
+      <h1 className="Welcome-text text-xs md:text-lg">
+        Creative Developer Portfolio
+      </h1>
     </motion.div>
+
+    <motion.div
+      variants={slideInFromLeft(0.5)}
+      className="flex flex-col gap-2 md:gap-6 mt-2 md:mt-6 text-2xl md:text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+    >
+      <span>
+        Providing
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-orange-700">
+          {" "}
+          the best{" "}
+        </span>
+        project experience
+      </span>
+    </motion.div>
+
+    <motion.p
+      variants={slideInFromLeft(0.8)}
+      className="text-base md:text-lg text-gray-400 my-2 md:my-5 max-w-[600px]"
+    >
+      I'm a Web Developer with experience in Website, Mobile, and Software development. Check out my projects and skills.
+    </motion.p>
+    <motion.a
+      variants={slideInFromLeft(1)}
+      className="py-1 md:py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+    >
+      Learn More!
+    </motion.a>
+  </div>
+
+  <motion.div
+    variants={slideInFromRight(0.8)}
+    className="w-full h-full flex justify-center items-center"
+  >
+    <Image
+      src="/mainIconsdark.svg"
+      alt="work icons"
+      height={400} // Decreased image height for smaller screens
+      width={400} // Decreased image width for smaller screens
+    />
+  </motion.div>
+</motion.div>
+
   );
 };
 
